0b37d0183397f096364308032044e5daf6228951
itsfiziks <gvanhoy@email.arizona.edu>
2015-01-24 17:14:18 -0700

    Added demoLecture2 files.

---
+++ demoLecture2.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=demoLecture2.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/demoLecture2.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=demolecture2.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/demolecture2.x.tar
